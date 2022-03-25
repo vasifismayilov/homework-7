b2b03af0c9723a11eb9ba45dab4365deda1a5097
vasifismayilov <ismayilov2265@gmail.com>
2022-03-25 21:29:12 +0400

    Homework7

---
+++ main.js
@@ -0,0 +1,49 @@
+// Funksiya run olundan 5 saniyə sonra konsolda bes saniye bitdi yazan bir funksiya hazırlayın
+
+
+// function finish() {
+//     console.log('Bes saniye bitdi');
+//   }
+  
+//   setTimeout(finish, 5000);
+
+
+
+
+// 65.4345 ədədini yuvarlaqlaşdırın
+
+// const num = Math.ceil(65.4345)
+// console.log(num)
+
+
+
+
+// 0 və 10 arasında random ədəd verən bir funksiya hazırlayın
+
+// const a = Math.random() * (10-1)
+// console.log(Math.ceil(a));
+
+
+
+
+
+// İndiki tarixə görə İl, ay, gün göstərən funksiya hazırlayın
+
+// let now = new Date();
+// console.log(now.getFullYear(),now.getMonth(),now.getDay());
+
+
+
+
+
+
+// İndiki tarixə görə saat, dəqiqə, saniyə göstərən funksiya hazırlayın
+
+// let now = new Date();
+// console.log(now.getHours(),now.getMinutes(),now.getSeconds())
+
+
+
+
+
+
